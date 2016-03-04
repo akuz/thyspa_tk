a97deafdc7c067871d1a98919dc25f063a62b2df
A Kuz <for.akuz@gmail.com>
2016-03-04 18:47:26 +0000

    Navbar explode.

--- header.php
+++ header.php
@@ -39,9 +39,7 @@
 
 
 				<div class="site-branding">
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-					<span><?php implode("</span> <span>", explode(" ", bloginfo( 'name' ))); ?></span>
-					</a></h1>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description lead"><?php bloginfo( 'description' ); ?></p>
 				</div>
 
@@ -66,7 +64,9 @@
 						</button>
 	
 						<!-- Your site title as branding in the menu -->
-						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+						<span><?php implode("</span> <span>", explode(" ", bloginfo( 'name' ))); ?></span>
+						</a>
 					</div>
 
 					<!-- The WordPress Menu goes here -->
