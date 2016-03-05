78a5cd3084d6956d8dbaa55b3fcd5ca702b0bc08
akuz <for.akuz@gmail.com>
2016-03-05 12:39:00 +0000

    Bar

--- header.php
+++ header.php
@@ -50,43 +50,43 @@
 
 <nav class="site-navigation">
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
-	<div class="container">
-		<div class="row">
-			<div class="site-navigation-inner col-sm-12">
-				<div class="navbar navbar-default navbar-fixed-top">
-					<div class="navbar-header">
-						<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-							<span class="sr-only"><?php _e('Toggle navigation','_tk') ?> </span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-	
-						<!-- Your site title as branding in the menu -->
-						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-						<span><?php echo implode("</span> <span>", explode(" ", get_bloginfo( 'name' ))); ?></span>
-						</a>
-					</div>
+	<div class="site-navigation-inner">
+		<div class="navbar navbar-default navbar-fixed-top">
 
-					<!-- The WordPress Menu goes here -->
-					<?php wp_nav_menu(
-						array(
-							'theme_location' 	=> 'primary',
-							'depth'             => 2,
-							'container'         => 'div',
-							'container_class'   => 'collapse navbar-collapse',
-							'menu_class' 		=> 'nav navbar-nav',
-							'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
-							'menu_id'			=> 'main-menu',
-							'walker' 			=> new wp_bootstrap_navwalker()
-						)
-					); ?>
+			<div class="container">
 
-				</div><!-- .navbar -->
+				<div class="navbar-header">
+					<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only"><?php _e('Toggle navigation','_tk') ?> </span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+
+					<!-- Your site title as branding in the menu -->
+					<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<span><?php echo implode("</span> <span>", explode(" ", get_bloginfo( 'name' ))); ?></span>
+					</a>
+				</div>
+
+				<!-- The WordPress Menu goes here -->
+				<?php wp_nav_menu(
+					array(
+						'theme_location' 	=> 'primary',
+						'depth'             => 2,
+						'container'         => 'div',
+						'container_class'   => 'collapse navbar-collapse',
+						'menu_class' 		=> 'nav navbar-nav',
+						'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
+						'menu_id'			=> 'main-menu',
+						'walker' 			=> new wp_bootstrap_navwalker()
+					)
+				); ?>
+				
 			</div>
-		</div>
-	</div><!-- .container -->
+		</div><!-- .navbar -->
+	</div>
 </nav><!-- .site-navigation -->
 
 <div class="main-content">
