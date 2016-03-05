aac3813ea5855939a5e6d8056dcecda24a8dca0e
akuz <for.akuz@gmail.com>
2016-03-05 14:20:28 +0000

    Move styles.

--- footer.php
+++ footer.php
@@ -1,12 +1,3 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the id=main div and all content after
- *
- * @package _tk
- */
-?>
 
 </div><!-- close .main-content -->
 
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-	<div class="container">
-
-		a
-
+	<div id="intro-video-container">
+		<div id="intro-video">
+			
+		</div>
 	</div>
 
 <?php get_footer(); ?>
