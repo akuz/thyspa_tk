0aa2068dffda623a5e0e5bc599dd92189fe04c44
akuz <for.akuz@gmail.com>
2016-03-06 13:43:30 +0000

    .

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 		<video autoplay loop poster="">
 		    <source src="<?php echo wp_get_attachment_url(54); ?>" type="video/mp4" />
 		</video>
-		<div class="ts-logo">
-			<div class="ts-logo-t">T</div>
-			<div class="ts-logo-t">S</div>
-		</div>
+	</div>
+	<div id="intro-logo-ts">
+		<div id="intro-logo-t">T</div>
+		<div id="intro-logo-s">S</div>
 	</div>
 
 <?php get_footer(); ?>
