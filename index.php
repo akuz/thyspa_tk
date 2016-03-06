163e040a7713d1e119e4065b57dafb46244dcb23
akuz <for.akuz@gmail.com>
2016-03-06 12:05:52 +0000

    .

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 	<div id="intro-video-container">
-		<video autoplay loop poster="polina.jpg" id="bgvid">
-		    <source src="<?php echo wp_get_attachment_url("beach_lowq"); ?>" type="video/mp4">
+		<video autoplay loop poster="" id="bgvid">
+		    <source src="<?php echo wp_get_attachment_url(50); ?>" type="video/mp4">
 		</video>
 	</div>
 
