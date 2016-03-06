c30e13d970f0647b0adf65de9a80cd13358fad6f
akuz <for.akuz@gmail.com>
2016-03-06 17:42:50 +0000

    .

--- index.php
+++ index.php
@@ -18,15 +18,13 @@
 		<div id="intro-welcome-big">Original Thai Massage</div>
 		<div id="intro-welcome-small">Coming Soon</div>
 	</div>
-	<div id="intro-slogan">
+	<div id="intro-info">
 		<h1>You deserve it.</h1>
 	</div>
 	<div id="intro-woman">
 	</div>
-
-	<div class="container">
-		<h1>Contact us</h1>
-
+	<div class="intro-info">
+		<h1>Contact us at</h1>
 	</div>
 
 <?php get_footer(); ?>
