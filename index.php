bf047fdc499374059d21d83987acd491535b2ed4
akuz <for.akuz@gmail.com>
2016-03-06 13:17:14 +0000

    .

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 	<div id="intro-video-container">
 		<video autoplay loop poster="" id="bgvid">
-		    <source src="<?php echo wp_get_attachment_url(50); ?>" type="video/mp4">
+		    <source src="<?php echo wp_get_attachment_url(52); ?>" type="video/mp4">
 		</video>
+		<div class="thyspa-logo">
 	</div>
 
 <?php get_footer(); ?>
