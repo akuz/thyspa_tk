d5ddb493cb3c0137d74f2bffc6ab487c9b292a32
A Kuz <for.akuz@gmail.com>
2016-03-07 21:54:45 +0000

    .

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 	</div>
 
 	<script type="text/javascript">
-  $(document).ready(function($) {
+	$(document).ready(function($) {
 
 		// init controller
 		var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
@@ -40,7 +40,7 @@
 						.setTween("#intro-woman > img", {y: "80%", ease: Linear.easeNone})
 						.addIndicators()
 						.addTo(controller);
-	}
+	});
 	</script>
 
 <?php get_footer(); ?>
