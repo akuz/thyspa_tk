8375c6a5cae12a98990cd5d1b1be8ce7006b423f
A Kuz <for.akuz@gmail.com>
2016-03-08 07:47:34 +0000

    .

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 
 		// build scenes
 		new ScrollMagic.Scene({triggerElement: "#intro-woman"})
-						.setTween("#intro-woman > img", {y: "80%", ease: Linear.easeNone})
+						.setTween("#intro-woman > img", {y: "80%"})
 						.addIndicators()
 						.addTo(controller);
 	});
