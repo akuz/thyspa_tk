ed536f22b9ebedef7527fd62e33e674745ed1fa8
root <root@ip-172-31-26-79.eu-west-1.compute.internal>
2016-06-08 22:21:28 +0000

    Latest changes.

--- footer.php
+++ footer.php
@@ -9,9 +9,13 @@
 				<h5><a href="https://www.facebook.com/thyspaoxford/">Facebook</a></h5>
 				<h5><a href="https://www.instagram.com/thy.spa/">Instagram</a></h5>
 				<h5><a href="https://twitter.com/thyspaoxford">Twitter</a></h5>
+				<h5>To receive latest news and offers, please <a href="http://eepurl.com/bS2S8j">subscribe</a> to our mailing list.</h5>
 			</div>
 			<div class="col-xs-6 col-sm-3">
-				<h4 class="ts-logo-font">Thy Spa</h4>
+				<h4 class="ts-logo-font ts-capitalize">
+					<span>Thy</span>
+					<span>Spa</span>
+				</h4>
 				<h5>Original Thai Massage</h5>
 				<h5>E: <a href="mailto:baz@thyspa.com">baz@thyspa.com</a></h5>
 				<h5>T: 078 0800 6810</h5>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<![endif]-->
 	<div id="intro-video-container">
 		<video autoplay loop poster="" id="intro-video">
-		    <source src="<?php echo wp_get_attachment_url(58); ?>" type="video/mp4" />
+		    <source src="<?php echo wp_get_attachment_url(55); ?>" type="video/mp4" />
 		</video>
 	</div>
 	<div id="intro-logo-ts">
@@ -16,12 +16,16 @@
 	</div>
 	<div id="intro-welcome">
 		<h1>Original Thai Massage</h1>
-		<h4>Coming Soon</h4>
+		<h4>Coming Soon in Oxford</h4>
 	</div>
 	<div class="intro-info">
+		<h3>Healthy Mind, Body, and Spirit</h3>
+		<!--
 		<h3>We are a group of professionals with great passion for spa & beauty industry, with our team including qualified Thai massage therapists with more than 5 years experience at big spas in the UK.</h3>
+		-->
 	</div>
-	<div id="intro-woman" class="parallax">
+
+	<div id="intro-woman" class="parallax-window" data-parallax="scroll" data-z-index="1" data-image-src="<?php echo wp_get_attachment_url(51); ?>">
 	    <img src="<?php echo wp_get_attachment_url(51); ?>" class="background" />
 	</div>
 
@@ -33,8 +37,6 @@
 	-->
 
 	<script type="text/javascript">
-
-  // var parallaxes = d3.select(".parallax");
 
 	</script>
 
